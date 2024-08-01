51d5df2ebe77bd4ce4890f75eaa6d5c3fe0034c9
stvmallen <esteban-mallen@users.noreply.github.com>
2024-08-01 20:54:43 -0300

    Using remote images for products

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,29 +1,20 @@
 import "./Item.css"
-import flower from "../../assets/placeholder-flower.png"
 import ItemDescription from "../ItemDescription/ItemDescription.jsx";
 import ItemImage from "../ItemImage/ItemImage.jsx";
-import {useState} from "react";
 
 const Item = ({ item, fullView }) => {
-    const [image, setImage] = useState(flower);
-    loadImage(item.category, item.id).then((itemImagePath) => setImage(itemImagePath));
+    const imagePath = `https://res.cloudinary.com/dsx9cbedb/image/upload/v1722556222/${item.id}.png`;
 
     return (
         <>
             <div className={"item"}>
                 <div className={"itemDisplay"}>
-                    <ItemImage image={image}/>
+                    <ItemImage image={imagePath}/>
                     <ItemDescription item={item} showDescriptionText={fullView}/>
                 </div>
             </div>
         </>
     )
-}
-
-const loadImage = async (itemCategory, itemId) => {
-    const imgPath = `/src/assets/products/${itemId}.png`;
-    await import(`../../assets/products/${itemId}.png`);
-    return imgPath;
 }
 
 export default Item
